a8396af7c53bbb5ec67d902d83d430e5505533fa
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-03-02 16:42:32 +0200

    Trajectory: `FiringRate`, Time: `2015_03_02_15h42m32s`, , Comment: `Experiment to measure the firing rate of a leaky integrate and fire neuron. Exploring different input currents, as well as refractory periods`

--- lifmodel/pypetwrapper.py
+++ lifmodel/pypetwrapper.py
@@ -100,7 +100,7 @@ def add_exploration(traj):
 
     print('Adding exploration of I and tau_ref')
 
-    explore_dict = {'neuron.I': np.arange(0, 1.01, 0.01).tolist(),
+    explore_dict = {'neuron.I': np.arange(0, 1.01, 0.05).tolist(),
                     'neuron.tau_ref': [5.0, 7.5, 10.0]}
 
     explore_dict = cartesian_product(explore_dict, ('neuron.tau_ref', 'neuron.I'))
